72f2f720d439399d44fe22249b8ed0dc99d232de
Sivaa Balamurugan <74814237+SivaaB@users.noreply.github.com>
2023-04-18 00:41:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,227 +1,202 @@
 import pickle
-
+from time import sleep
+from random import random
+from threading import Thread  
 # Create a dictionary to store usernames and passwords
-
 users = {}
-
 company={}
-
 # Function to save the user dictionary to a binary file
-
 def save_users():
-
-    with open("users.bin", "wb") as f:
-
-        pickle.dump(users, f)
-
+    with open("users.bin", "wb") as f:
+        pickle.dump(users, f)
 
 # Function to load the user dictionary from the binary file
-
 def load_users():
+    try:
+        with open("users.bin", "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return {}
 
-    try:
-
-        with open("users.bin", "rb") as f:
-
-            return pickle.load(f)
-
-    except FileNotFoundError:
-
-        return {}
-
-
-# Load the existing users from the binary file
-
-users = load_users()
-
-
+# Function to save the company dictionary to a binary file
 def save_company():
-
-    with open("company.bin", "wb") as f:
-
-        pickle.dump(company,f)
-
+    with open("company.bin", "wb") as f:
+        pickle.dump(company,f)
 
 def load_company():
-
-    try:
-
-        with open("company.bin", "rb") as f:
-
-            return pickle.load(f)
-
-    except FileNotFoundError:
-
-        return {}
+    try:
+        with open("company.bin", "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return {}
 
 
+def upload(filename):
+    with open("web.bin", "wb") as f:
+        pickle.dump(company[filename], f)
+def load_web():
+    try:
+        with open("web.bin", "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return {}
+
+web={}
+web=load_web()
+
+# Load the existing users from the binary file
+users = load_users()
 company=load_company()
 
+def save_mail(usera,subject,data):
 
-def upload():
-
-    with open("web.bin", "wb") as f:
-
-        pickle.dump(company[filename], f)
-
-
-def scan():
-
-    
+    globals()[usera] = {}
+    a=globals()[usera]
+    a[subject]=data
+    print(type(a))
+    with open(usera,"wb") as f:
+        pickle.dump(a,f)
 
 
+def load_mail(user):
+    a=globals()[user]
+    try:
+        with open(user,"rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return {}
 
 
+def view_mail(user):
+    a=load_mail(user)
+    for key in a:
+        print(key)
+    subject=input('enter name of mail to open')
+    print(a[subject])
+
+#submenu3
+def submenu3():
+    while True:
+        print("Submenu 3:")
+        print("1. send mail")
+        print("2. recieve mail")
+        print("3. quit")
+
+        choice=int(input("Enter your choice: "))
+        if choice==1:
+            print('send mail')
+            print('select the user you want to send the mail to')
+            for keys in users:
+                print(keys)
+            usera = input("Enter a username ")
+            subject=input('enter the subject')
+            data=input("enter your data ")
+            save_mail(usera,subject,data)
+        elif choice==2:
+            print('recieve mail')
+            user=input('enter your username')
+            view_mail(user)
+        elif choice==3:
+            break
+        else:
+            print("Invalid choice")
 
 
-def main_menu():
-
-    print("Main Menu:")
-
-    print("1. Sign-in")
-
-    print("2. Login")
-
-    print("3. Exit")
-
-    choice = int(input("Enter your choice: "))
-
-    if choice == 1:
-
-        username = input("Enter your username: ")
-
-
-        if username not in users:
-
-            password = input("Enter your password: ")
-
-            users[username] = password
-
-            save_users()
-
-            print("User created!")
-
-        else:
-
-            print('User already exists')
-
-    elif choice == 2:
-
-        username = input("Enter your username: ")
-
-
-        if username in users:
-
-            password = input("Enter your password: ")
-
-            # If the username exists, check if the password matches
-
-            if users[username] == password:
-
-                print("Login successful!")
-
-                submenu()
-
-            else:
-
-                print("Incorrect password")
-
-        else:
-
-            print('User does not exist')
-
-    elif choice == 3:
-
-        exit()
-
-    else:
-
-        print("Invalid choice.")
-
-        main_menu()
-
-
-
-
+#submenu2
 def submenu2():
+    while True:
+        print("Submenu 2:")
+        print("1. mail")
+        print("2. create")
+        print("3. open")
+        print("4. Back to previuos menu")
 
-    print("Submenu 2:")
+        choice = int(input("Enter your choice: "))
 
-    print("1. mail")
+        if choice == 1:
+            submenu3()
+        elif choice == 2:
+            print("create")
+            filename = input("Enter your filename: ")
+            if filename not in company:
+                data = input("Enter your data: ")
+                company[filename] = data
+                save_company()
+                print("File created!")
+            else:
+                print('Filename already exists')
+        elif choice == 3:
+            print('open')
+            for key in company.keys():
+                print(key)
+            filename=input('enter filename to open')
+            print(company[filename])
+            reply=input('Do you want to upload it to the web, reply with yes or no')
+            if reply=='yes':
+                upload(filename)
+                sleep(10)
+            elif reply=='no':
+                break
+                #exit to list of files
+            else:
+                print('you have entered incorrect option')
+        elif choice==4:
+            main_menu()
+        else:
+            print("Invalid choice.")
 
-    print("2. create")
+#main menu
+def main_menu():
+    while True:
+        print("Main Menu:")
+        print("1. Sign-in")
+        print("2. Login")
+        print("3. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            username = input("Enter your username: ")
+            if username not in users:
+                password = input("Enter your password: ")
+                users[username] = password
+                save_users()
+                with open (username,"w") as f:
+                    f.close()
+                print("User created!")
+            else:
+                print('User already exists')
+        elif choice == 2:
+            username = input("Enter your username: ")
+            if username in users:
+                password = input("Enter your password: ")
+                # If the username exists, check if the password matches
+                if users[username] == password:
+                    print("Login successful!")
+                    submenu2()
+                else:
+                    print("Incorrect password")
+            else:
+                print('User does not exist')
+        elif choice == 3:
+            exit()
+        else:
+            print("Invalid choice.")
 
-    print("3. open")
+def background_task():
+    global company
+    global web
+    comp=company
+    while True:
+        for key in comp:
+            for sub  in web:
+                if key==sub:
+                    if comp[key]['status']=='online':
+                        usera = 'admin'
+                        subject='leak detected'
+                        data="there has been a leak of foldet {key}"
+                        print("leak detected")
+                        save_mail(usera,subject,data)
+        sleep(0.1)
 
-    print("4. Back to previuos menu")
-
-
-    choice = int(input("Enter your choice: "))
-
-
-    if choice == 1:
-
-        print("mail")
-
-        
-
-    elif choice == 2:
-
-        print("create")
-
-        filename = input("Enter your filename: ")
-
-
-        if filename not in company:
-
-            data = input("Enter your data: ")
-
-            company[filename] = data
-
-            save_comapny()
-
-            print("File created!")
-
-        else:
-
-            print('Filename already exists')
-
-
-    elif choice == 3:
-
-        print('open')
-
-        for key in company.keys():
-
-            print(key)
-
-        filename=input('enter filename to open')
-
-        print(company[filename])
-
-        reply=input('Do you want to upload it to the web, reply with yes or no')
-
-        if reply=='yes':
-
-            upload()
-
-        elif reply=='no':
-
-            #exit to list of files
-
-        else:
-
-            print('you have entered incorrect option')
-
-
-    elif choice==4:
-
-        main_menu()
-
-    else:
-
-        print("Invalid choice.")
-
-        submenu2()
-
+daemon = Thread(target=background_task, daemon=True, name='Monitor')
+daemon.start()
 main_menu()
